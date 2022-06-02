e7f4549e6895e51c6bf23f22d28a1d7b5107a199
HarshithaMaruthi <88500915+HarshithaMaruthi@users.noreply.github.com>
2022-06-02 09:37:46 +0530

    sixth commit

---
+++ Day04/src/Cricketer.java
@@ -0,0 +1,21 @@
+
+ class Cricketer {
+	 String name = "Dhoni";
+	 int age = 39;
+	 String color ="Brown";
+	 String role ="Batsman";
+	 String nationality ="Indian";
+	 
+	 void Display() {
+		 System.out.println("I LOVE cricket");
+	 }
+	 public static void main(String [] args) {
+		 Demo myObj1= new Demo();
+		 Demo myObj2= new Demo();
+		 myObj1.x = 24;
+		 myObj2.x = 55;
+		 System.out.println(myObj1.x);
+		 System.out.println(myObj2.x);
+	
+	 }
+}
