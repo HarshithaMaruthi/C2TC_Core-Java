69d496c52ab15a749a855f8c2677170b4c28db14
HarshithaMaruthi <88500915+HarshithaMaruthi@users.noreply.github.com>
2022-06-16 14:05:17 +0530

    13th commit

---
+++ Interface/src/com/cg/Interface/Main.java
@@ -0,0 +1,13 @@
+package com.cg.Interface;
+
+public class Main {
+	
+		public static void main(String[] args) {
+			Animal donkey = new Animal("Donkey",2,56,4,34);
+			donkey.eyes();
+			donkey.teeths();
+			donkey.lamb();
+		}
+
+	}
+
