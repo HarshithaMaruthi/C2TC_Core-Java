20d45eeedf4dad9ccea38c8f92e1c881e1f6607f
HarshithaMaruthi <88500915+HarshithaMaruthi@users.noreply.github.com>
2022-06-16 14:08:05 +0530

    15th commit

---
+++ CollectionFrameWork/src/com/tns/CollectionFrameWork/CollectionFrameWork.java
@@ -0,0 +1,33 @@
+package com.tns.CollectionFrameWork;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+public class CollectionFrameWork {
+	
+		public static void main(String[] args) {
+			List<Integer> list=new ArrayList<Integer>();
+			list.add(null);
+		  list.add(1);
+		  list.add(2);  
+		  list.add(11);
+		  list.add(11);
+		  list.add(100);
+		  list.remove(2);
+		  for(Integer i: list) {//for each loop to iterate through the collection
+			  System.out.println(i);
+		  }
+		  for(int j=0;j<list.size();j++) {//simple for loop 2nd way
+			  System.out.println(list.get(j));
+		}
+		 Iterator z = list.iterator();
+		 while(z.hasNext()) {
+			 System.out.println(z.next());
+		 }
+	}
+	}
+
+
+
+
