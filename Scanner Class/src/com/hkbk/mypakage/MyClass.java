d751340ad8c0b065fe80ffb253f03596e44e9efc
HarshithaMaruthi <88500915+HarshithaMaruthi@users.noreply.github.com>
2022-06-20 14:15:48 +0530

    first commit

---
+++ Scanner Class/src/com/hkbk/mypakage/MyClass.java
@@ -0,0 +1,5 @@
+package com.hkbk.mypakage;
+
+public class MyClass {
+
+}
--- Scanner Class/src/com/hkbk/mypakage/PrintName.java
+++ Scanner Class/src/com/hkbk/mypakage/PrintName.java
@@ -1,11 +1,11 @@
 package com.hkbk.mypakage;
 
-import com.hkbk.mypakage.MyClass;
+import ;
 public class PrintName {
 
 	public static void main(String[] args) {
 		String name= "welcome to C2TC Program";
-		MyClass obj = new MyClass
+		MyClass obj = new MyClass;
 
 	}
 
